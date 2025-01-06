c13adcc824ee06ef8632f21e6f1d932993b25711
SwarnkarAdda <121598988+SwarnkarAdda@users.noreply.github.com>
2025-01-06 18:00:14 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -2,34 +2,6 @@ import streamlit as st
 import numpy as np
 
 
-# def calculate_mean(values):
-#     try:
-#         # Convert the list of strings to floats
-#         numeric_values = [float(value) for value in values]
-#         return np.mean(numeric_values)
-#     except ValueError:
-#         st.error("Please enter valid numeric values.")
-#         return None
-#
-#
-# # Streamlit app
-# st.title("Mean Calculator")
-# st.write("Enter a list of numeric values separated by commas, and we'll calculate the mean for you!")
-#
-# # User input
-# user_input = st.text_input("Enter values (comma-separated):")
-#
-# if user_input:
-#     # Split input into a list of values
-#     value_list = user_input.split(',')
-#
-#     # Calculate the mean
-#     mean_value = calculate_mean(value_list)
-#
-#     if mean_value is not None:
-#         st.success(f"The mean of the given values is: {mean_value}")
-#         st.success(f"value are: {value_list}")
-
 
 from scipy import stats
 st.title("Descriptive analytics")
@@ -50,10 +22,6 @@ try:
     st.write(f"Standard Deviation: {np.std(f)}")
     st.write("---")
     st.write(f"Variance: {np.var(f)}")
-
-
-
-
 
 
 except ValueError:
